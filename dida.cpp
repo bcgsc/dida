9152250ef4fca9a86c38ecc06ebadfef0dba0d63
mohamadi <mohamadi.hamid@gmail.com>
2013-07-31 11:29:55 -0700

    "Modifying argument parsing."

--- dida.cpp
+++ dida.cpp
@@ -1,11 +1,99 @@
 #include <mpi.h>
 #include <cstdlib>
-
+#include <getopt.h>
+#include <algorithm>
+#include <iterator>
 #include "partition.h"
 #include "merge.h"
 
-int main(int argc, char *argv[]){
-	    
+
+#define PROGRAM "dida"
+
+static const char VERSION_MESSAGE[] =
+PROGRAM " Version 0-1.1 \n"
+"Written by Hamid Mohamadi.\n"
+"Copyright 2013 Canada's Michael Smith Genome Science Centre\n";
+
+static const char USAGE_MESSAGE[] =
+"Usage: " PROGRAM " [OPTION]... QUERY... TARGET\n"
+"Map the sequences of the files QUERY to those of the file TARGET.\n"
+"The index files TARGET.fai and TARGET.fm will be used if present.\n"
+"\n"
+" Options:\n"
+"\n"
+"  -p, --partition=N       divide reference to N partitions\n"
+"  -l, --min-align=N       find matches at least N bp [1]\n"
+"  -j, --threads=N         use N parallel threads [1]\n"
+"  -b, --bfl=N         use N bp for Bloom filter [1]\n"
+"      --help              display this help and exit\n"
+"      --version           output version information and exit\n"
+"\n"
+"Report bugs to hmohamadi@bcgsc.ca.\n";
+
+static const char shortopts[] = "p:l:b:j:v";
+
+enum { OPT_HELP = 1, OPT_VERSION };
+
+static const struct option longopts[] = {
+	{ "partition", required_argument, NULL, 'p' },
+	{ "min-align", required_argument, NULL, 'l' },
+    { "bfl", required_argument, NULL, 'b' },
+	{ "threads", required_argument, NULL, 'j' },
+	{ "help", no_argument, NULL, OPT_HELP },
+	{ "version", no_argument, NULL, OPT_VERSION },
+	{ NULL, 0, NULL, 0 }
+};
+
+int main(int argc, char **argv){
+    
+    int pNum, alnLmer,bflKmer,numThr;
+    
+    
+    std::string commandLine;
+    {
+        std::ostringstream ss;
+		char** last = argv + argc - 1;
+		copy(argv, last, std::ostream_iterator<const char *>(ss, " "));
+		ss << *last;
+		commandLine = ss.str();
+	}
+    
+    bool die = false;
+	for (int c; (c = getopt_long(argc, argv, shortopts, longopts, NULL)) != -1;) {
+        std::istringstream arg(optarg != NULL ? optarg : "");
+		switch (c) {
+			case '?': die = true; break;
+			case 'j': arg >> numThr; break;
+			case 'l': arg >> alnLmer;break;
+			case 'b': arg >> bflKmer; break;
+			case 'p': arg >> pNum; break;
+			case OPT_HELP:
+                std::cout << USAGE_MESSAGE;
+				exit(EXIT_SUCCESS);
+			case OPT_VERSION:
+                std::cout << VERSION_MESSAGE;
+				exit(EXIT_SUCCESS);
+		}
+		if (optarg != NULL && !arg.eof()) {
+            std::cerr << PROGRAM ": invalid option: `-"
+            << (char)c << optarg << "'\n";
+			exit(EXIT_FAILURE);
+		}
+	}
+	if (argc - optind != 2) {
+        std::cerr << PROGRAM ": missing arguments\n";
+		die = true;
+	}
+    
+	if (die) {
+        std::cerr << "Try `" << PROGRAM
+        << " --help' for more information.\n";
+		exit(EXIT_FAILURE);
+	}
+    
+    const char *readName(argv[argc-2]);
+    const char *fasName(argv[argc-1]);
+    
 	int procSize, procRank, prcrNlen;
 	char processor_name[MPI_MAX_PROCESSOR_NAME];
 
@@ -16,13 +104,6 @@ int main(int argc, char *argv[]){
 	MPI_Get_processor_name(processor_name,&prcrNlen);
 	   
 	printf("Starting process %d out of %d on %s\n", procRank, procSize, processor_name);
-
-	int pNum = atoi(argv[1]); // Number of Partitions
-	int alnLmer = atoi(argv[2]); // Alignment censecutive match length
-	int bflKmer=atoi(argv[3]); // Bloom filter kmer length
-	int numThr=atoi(argv[4]); // Number of threads 
-	const char *readName = argv[5]; // Name of read file in fastq format
-	const char *fasName = argv[6]; // Name of Refrence, Contig file in fasta format
 
 	std::string ubName, ueName;
 	getFname(fasName, ubName, ueName);
