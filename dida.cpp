a899346ebc873437ab9fe0b92fb176a0def109c7
Ben Vandervalk <benv@bcgsc.ca>
2014-12-06 17:14:25 -0800

    Common/MurmurHash2.h: move murmur hash to its own header file

--- dida.cpp
+++ dida.cpp
@@ -1,6 +1,7 @@
 #include "config.h"
 #include "Uncompress.h"
 #include "FileUtil.h"
+#include "MurmurHash2.h"
 #include "prt.h"
 
 #include <mpi.h>
@@ -13,7 +14,6 @@
 #include <getopt.h>
 #include <assert.h>
 #include <stdio.h>
-#include <stdint.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
@@ -152,50 +152,6 @@ size_t getInfo(const char *aName, unsigned k){
 	std::cerr << "|totLen|=" << totItm << std::endl;
 	faFile.close();
 	return totItm;
-}
-
-// MurmurHash2, 64-bit versions, by Austin Appleby
-// https://sites.google.com/site/murmurhash/MurmurHash2_64.cpp?attredirects=0
-uint64_t MurmurHash64A ( const void * key, int len, unsigned int seed ){
-	const uint64_t m = 0xc6a4a7935bd1e995;
-	const int r = 47;
-
-	uint64_t h = seed ^ (len * m);
-
-	const uint64_t * data = (const uint64_t *)key;
-	const uint64_t * end = data + (len/8);
-
-	while(data != end)
-	{
-		uint64_t k = *data++;
-
-		k *= m;
-		k ^= k >> r;
-		k *= m;
-
-		h ^= k;
-		h *= m;
-	}
-
-	const unsigned char * data2 = (const unsigned char*)data;
-
-	switch(len & 7)
-	{
-        case 7: h ^= uint64_t(data2[6]) << 48;
-        case 6: h ^= uint64_t(data2[5]) << 40;
-        case 5: h ^= uint64_t(data2[4]) << 32;
-        case 4: h ^= uint64_t(data2[3]) << 24;
-        case 3: h ^= uint64_t(data2[2]) << 16;
-        case 2: h ^= uint64_t(data2[1]) << 8;
-        case 1: h ^= uint64_t(data2[0]);
-	        h *= m;
-	};
-
-	h ^= h >> r;
-	h *= m;
-	h ^= h >> r;
-
-	return h;
 }
 
 void filInsert(std::vector< std::vector<bool> > &myFilters, const unsigned pn, const std::string &bMer) {
